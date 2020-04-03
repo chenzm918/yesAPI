102e733b24a19b3c9e16c3cd1b94d437dac2e619
chenzm <304554088@qq.com>
2020-04-03 10:51:22 +0800

    Initial commit

---
+++ lib/util.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author:chenzhimin
+@software:APITest
+@file:util.py
+@time:2020/4/3  10:47
+@desc
+'''
+import hashlib
+
+def hash_code(pwd):
+    md5 = hashlib.md5()
+    md5.update(pwd.encode('utf-8'))
+    return md5.hexdigest()
+
+
+
+def set_res_data(res):
+    if res:
+        return res.replace('":"',"=").replace('":',"=")
---
+++ model/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author:chenzhimin
+@software:APITest
+@file:__init__.py.py
+@time:2020/4/3  10:43
+@desc
+'''
