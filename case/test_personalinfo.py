e9e26bc591217a30a881ab874ff3238be80e1c47
chenzm <304554088@qq.com>
2020-04-07 14:32:28 +0800

    修改获取个人信息的用例，添加了备注，删除冗余的部分

--- case/test_personalinfo.py
+++ case/test_personalinfo.py
@@ -7,12 +7,18 @@
 @time:2020/4/3  16:02
 @desc
 '''
+
 import unittest
 import ddt
 import os
 from model.setting import*
 from lib.util import *
 import requests
+
+#接口说明：获取会员个人资料接口
+#获取用户自己的账号/昵称、注册时间以及是否为管理员、扩展信息等信息
+#必须要传的参数有app_key、uuid、token
+#app_key长度为32、uuid长度为32、token长度为64
 
 
 @ddt.ddt
@@ -27,31 +33,22 @@ class Personalinfo(unittest.TestCase):
         _token = case.get('_token')#获取_token中的值。为后面判断提供依据
         _uuid = case.get('_uuid')#获取_token中的值。为后面判断提供依据
         check = case.get('check')
+        login_app_key = case.get("data_login").get('login_app_key')  # 获取的是登录data中的app_key
+        username = case.get("data_login").get('username')  # 获取的是登录data中的username
+        password = case.get("data_login").get('password')  # 获取的是登录data中的password
+        login_url = case.get("data_login").get('login_url')  # 获取的是登录的url
+        uuid_,token_ = get_token(login_url, login_app_key, username, password)  # 获取动态的token
+
         self._testMethodDoc = case.get('doc', '无')
         if _token == True and _uuid==True:
-            login_app_key = case.get("data_login").get('login_app_key')#获取的是登录data中的app_key
-            username = case.get("data_login").get('username')#获取的是登录data中的username
-            password = case.get("data_login").get('password')#获取的是登录data中的password
-            login_url = case.get("data_login").get('login_url')#获取的是登录的url
-            _token = get_token(login_url, login_app_key, username, password)[1]#获取动态的token
-            _uuid = get_token(login_url, login_app_key, username, password)[0]#获取动态的uuid
-            dic = {'app_key': app_key, 'uuid': _uuid, 'token': _token}
+            dic = {'app_key': app_key, 'uuid': uuid_, 'token': token_}
             result = requests.post(url=url, data=dic)
         elif _uuid == False:
-            login_app_key = case.get("data_login").get('login_app_key')#获取的是登录data中的app_key
-            username = case.get("data_login").get('username')#获取的是登录data中的username
-            password = case.get("data_login").get('password')#获取的是登录data中的password
-            login_url = case.get("data_login").get('login_url')#获取的是登录的url
-            _token = get_token(login_url, login_app_key, username, password)[1]#获取动态的uuid
-            dic = {'app_key': app_key, 'uuid': uuid, 'token': _token}
+
+            dic = {'app_key': app_key, 'uuid': uuid, 'token': token_}
             result = requests.post(url=url, data=dic)
         elif _token == False:
-            login_app_key = case.get("data_login").get('login_app_key')#获取的是登录data中的app_key
-            username = case.get("data_login").get('username')#获取的是登录data中的username
-            password = case.get("data_login").get('password')#获取的是登录data中的password
-            login_url = case.get("data_login").get('login_url')#获取的是登录的url
-            _uuid = get_token(login_url, login_app_key, username, password)[0]#获取动态的token
-            dic = {'app_key': app_key, 'uuid': _uuid, 'token':token}
+            dic = {'app_key': app_key, 'uuid': uuid_, 'token':token}
             result = requests.post(url=url, data=dic)
         else:
             result = requests.post(url=url, data=data)
