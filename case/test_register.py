8587e1fc8d21fa77a5ec3b8c5b6ad4c47a2ea3ef
chenzm <304554088@qq.com>
2020-04-07 14:30:46 +0800

    修改注册的用例，添加了备注信息

--- case/test_register.py
+++ case/test_register.py
@@ -13,7 +13,14 @@ import os
 from model.setting import*
 from lib.util import *
 import requests
-
+#接口说明：用户注册接口
+#进行新用户注册，创建一个新的会员账号，注册密码需要使用md5后的密码。
+#必须要传的参数有app_key、username、password
+#选填参数：uuid、token、ext_info
+#密码必须md5加密
+#app_key长度为32、uuid长度为32、token64位、usename(1,50)
+# ext_info注册时的用户扩展信息，注册后可修改，需要JSON编码后传递。
+# 格式：ext_info={"扩展字段名":"值"}，可以同时更新多个字段。
 
 @ddt.ddt
 class Register(unittest.TestCase):
@@ -28,8 +35,6 @@ class Register(unittest.TestCase):
         if MD_5 == True:
             if 'password' in data:
                 data['password'] = hash_code(data['password'])
-
-
         result = requests.post(url=url,data=data)
         ass = set_res_data(result.text)
         for c in check:
