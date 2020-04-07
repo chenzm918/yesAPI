7e0fa2d160e4c341453829d478404bda7122f214
chenzm <304554088@qq.com>
2020-04-07 14:02:38 +0800

    批量运行用例的文件

--- bin/run.py
+++ bin/run.py
@@ -9,9 +9,12 @@
 '''
 import unittest
 from BeautifulReport import BeautifulReport
+
+from lib.set_case_file import create_case_file
 from model.setting import *
 
-
+#首先生成测试用例
+# create_case_file()
 
 
 file_name = '{}接口测试.html'.format(TIME)
