06a99232f0854024ae22a0f9257416acf6608332
dyl <30455258@QQ.COM->
2020-12-27 18:31:33 +0800

    修改

--- bin/run.py
+++ bin/run.py
@@ -6,16 +6,18 @@
 @file:run.py
 @time:2020/4/2  22:45
 @desc
-'''
-import unittest
-from BeautifulReport import BeautifulReport
-from model.setting import *
-import time
+# '''
 
-
-
-file_name = '{}接口测试.html'.format(TIME)
-#选择要执行的用例
-discover =unittest.defaultTestLoader.discover(CASE_PATH,'test*.py')
-
-BeautifulReport(discover).report(description='接口测试',filename=file_name,log_path=REPORT_PATH)
+print ('hhh')
+# import unittest
+# from BeautifulReport import BeautifulReport
+# from model.setting import *
+# import time
+#
+#
+#
+# file_name = '{}接口测试.html'.format(TIME)
+# #选择要执行的用例
+# discover =unittest.defaultTestLoader.discover(CASE_PATH,'test*.py')
+#
+# BeautifulReport(discover).report(description='接口测试',filename=file_name,log_path=REPORT_PATH)
